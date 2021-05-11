d3df3f73df15310b647449d824f8c0ec23cd5526
Henrique1818 <luiz.henrique01827@gmail.com>
2021-05-11 11:36:50 -0300

    Funcionamento de uma callback no Nodejs

---
+++ 02-nodejs/index.js
@@ -0,0 +1,63 @@
+/*
+ 0 Obter um usuario
+ 1 Obter o numero de telefone do usuario a partir de um id
+ 2 Obter o endereco do usuario pelo id
+*/
+
+function obterUsuario(callback) {
+    setTimeout(function() {
+        return callback(null, {
+            id: 1,
+            nome: 'Aladin',
+            dataNascimento: new Date()
+        });
+    }, 1000);
+}
+
+function obterTelefone(idUsuario, callback) {
+    setTimeout(function() {
+        return callback (null, {
+            telefone: '1299003',
+            ddd: 11
+        })
+    }, 2000);
+}
+
+function obterEndereco(idUsuario, callback) {
+    setTimeout(() => {
+        return callback(null, {
+            rua: 'dos bobos',
+            numero: 0
+        });
+    }, 2000);
+}
+
+function resolverUsuario(erro, usuario) {
+    console.log('usuario', usuario);
+}
+
+obterUsuario(function resolverUsuario(error, usuario) {
+    if(error) {
+        console.error('DEU RUIM em USUARIO', error);
+        return;
+    }
+
+    obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
+        if(error1) {
+            console.error('DEU RUIM em TELEFONE', error1);
+            return;
+        }
+        obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
+            if(error2) {
+                console.error('DEU RUIM em ENDERECO', error2);
+                return;
+            }
+
+            console.log(`
+                Nome: ${usuario.nome},
+                Endereço: ${endereco.rua}, n°: ${endereco.numero},
+                Telefone: (${telefone.ddd})${telefone.telefone}
+            `);
+        });
+    })
+});
